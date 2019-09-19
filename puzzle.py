5fd5ab8ad51e690f6d5b409893890c1de1bfdeca
Arié Selinger <arie.selinger@gmail.com>
2019-09-19 00:01:53 -0700

    Update: puzzle class permute

--- puzzle.py
+++ puzzle.py
@@ -4,6 +4,7 @@ import utils
 import matplotlib.pyplot as plt
 import matplotlib.image as image
 from PIL import Image
+from sympy.combinatorics import Permutation
 
 from config import SAVE_FOLDER, HEIGHT, WIDTH, N_ROW, N_COL, HEIGHT_BLOCK, WIDTH_BLOCK
 
@@ -32,6 +33,9 @@ class Puzzle:
         # Create cut_image with shape (N_ROW, N_COL, HEIGHT_BLOCK, WIDTH_BLOCK)
         self.cut_img = utils.cut_image(self.original_img)
     
+    def reinit(self):
+        self.cut_img = utils.cut_image(self.original_img)
+
     def print(self):
         '''
         Shows the whole puzzle
@@ -56,13 +60,50 @@ class Puzzle:
         np.random.shuffle(self.cut_img.reshape(N_ROW*N_COL, HEIGHT_BLOCK, WIDTH_BLOCK))
         self.cut_img.reshape(N_ROW, N_COL, HEIGHT_BLOCK, WIDTH_BLOCK)
 
+
+    def permute(self, k=None):
+        '''
+        permute the blocks with less than k transpositions
+
+        k : number of transpositions
+
+        return: True number of transpositions
+
+        '''
+        if k == None:
+            p = np.random.permutation(N_ROW*N_COL)
+        else: 
+            p = []
+            for _ in range(k):
+                a = 0
+                b = 0
+                while(a == b):
+                    a = np.random.randint(0,N_ROW*N_COL-1)
+                    b = np.random.randint(0,N_ROW*N_COL-1)
+                p.append((a,b))
+        p = Permutation(p, size=N_ROW*N_COL)
+
+        self.cut_img = self.cut_img.reshape(N_ROW*N_COL, HEIGHT_BLOCK, WIDTH_BLOCK)
+        original = self.cut_img.copy()
+
+        for k in range(N_ROW*N_COL):
+            self.cut_img[k] = original[p(k)]
+
+        self.cut_img = self.cut_img.reshape(N_ROW, N_COL, HEIGHT_BLOCK, WIDTH_BLOCK)
+
+        return len(p.transpositions())
+
     def move(self, i1, j1, i2,j2):
         block1 = self.cut_img[i1][j1].copy()
         block2 = self.cut_img[i2][j2].copy()
         self.cut_img[i1,j1], self.cut_img[i2,j2] = block2, block1
     
-    def save(self, filename):
-        filepath = os.path.join(SAVE_FOLDER, filename)
+    def save(self, filename=None, pathname=None):
+        if pathname == None:
+            filepath = os.path.join(SAVE_FOLDER, filename)
+        else:
+            filepath = os.path.join(pathname)
+        
         if not os.path.exists(filepath):
             to_save_img = utils.recreate_cut_image(self.cut_img)
             plt.imsave(filepath, utils.recreate_cut_image(self.cut_img), cmap="gray")
